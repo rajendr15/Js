cf989f9a48b1acb3a3acd571053333f6c747ad42
rajendr15 <60357806+rajendr15@users.noreply.github.com>
2023-09-17 17:12:44 +0000

    my starting in js

---
+++ 01_basics/07_basics.js
@@ -0,0 +1,9 @@
+// Operations
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+let str1=4
+let str2=5
+let str3= str1 + str2
+console.log(str3);
---
+++ 01_basics/test.js
@@ -0,0 +1,7 @@
+const accountId = 2190
+let accountEmail = "jaifrajendra"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2 not allowed
+
+console.log(accountId)
