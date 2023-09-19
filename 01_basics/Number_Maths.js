532b55f3e8d5edfcf32f4c5be9a59e0bea8344fa
rajendr15 <60357806+rajendr15@users.noreply.github.com>
2023-09-19 11:01:23 +0000

    math and  num

---
+++ 01_basics/Number_Maths.js
@@ -0,0 +1,27 @@
+const Score=400
+// console.log(Score)
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8456
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+// +++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(3,4,1,56,7));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1 );
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
---
+++ 01_basics/String.js
@@ -0,0 +1,13 @@
+const name ="rajendra"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh')
+console.log(gameName[2]);
+console.log(gameName._proto_);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+const anthoerString = gameName.slice(-8,4)
