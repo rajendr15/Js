3c372d08e654033b3af4acb383fd5a3dc0802cd0
rajendr15 <60357806+rajendr15@users.noreply.github.com>
2023-09-19 07:08:05 +0000

    data types of js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+// #Primitive 
+//7 types : String,Number,Boolean , null ,undefind, Symbol ,BigInt
+const score =100
+let outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId)
+const bigNumber = 12345671234562345n
+
+// REference (Non Perimitive)
+
+
+// Array, Objects,Functions 
+const heros = ["shaktiman", "naagraj","doga"];
+
+let myObj = {
+    name: "rajendra",
+    age: 22,
+}
+
+const myFunction = function(){
+      console.log("Hello world");
+}
+
